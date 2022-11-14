00784bcf4b03c8599b5a76d347859e5264ce31fb
Stanghi <stangherlin.matti@gmail.com>
2022-11-14 14:59:51 +0100

    print h1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+    name: 'App',
+    components: {
+        AppTitle,
+    },
+};
+</script>
+
+<template>
+    <AppTitle />
+</template>
+
+<style></style>
